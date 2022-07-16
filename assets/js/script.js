35875a83dbee6bd025c05d9afd245665a3814120
leah-jala <leahlafond@protonmail.com>
2022-07-16 11:04:56 +0000

    Add event listeners for document loading and buttton clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-function runGame(){
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button"); //returns all the elements it found as an array
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") { //"This" refers to the button that was just clicked. 
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
 
 }
 
@@ -6,7 +24,7 @@ function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer(){
+function calculateCorrectAnswer() {
 
 }
 
